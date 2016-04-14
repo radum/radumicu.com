36244d37cc4ea5f37ea7e048f56dd58ca08da390
RaduM <radu.micu@digitaslbi.com>
2016-04-14 18:08:37 +0100

    gulp consolidate, future code updates, still WIP

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -35,121 +35,6 @@ function mapError(err) {
 	}
 }
 
-function lint(files, options) {
-	return () => {
-		return gulp.src(files)
-			.pipe(reload({ stream: true, once: true }))
-			.pipe($.eslint(options))
-			.pipe($.eslint.format())
-			.pipe($.if(!browserSync.active, $.eslint.failAfterError()));
-	};
-}
-const testLintOptions = {
-	env: {
-		mocha: true
-	}
-};
-
-gulp.task('lint', lint('app/scripts/**/*.js'));
-gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
-
-gulp.task('html', ['styles', 'scripts'], () => {
-	return gulp.src('app/*.html')
-		.pipe($.useref({ searchPath: ['.tmp', 'app', '.'] }))
-		.pipe($.if('*.js', $.uglify()))
-		.pipe($.if('*.css', $.cssnano({ safe: true, autoprefixer: false })))
-		.pipe($.if('*.html', $.htmlmin({ collapseWhitespace: true })))
-		.pipe(gulp.dest('dist'));
-});
-
-gulp.task('images', () => {
-	return gulp.src('app/images/**/*')
-		.pipe($.cache($.imagemin({
-			progressive: true,
-			interlaced: true,
-			// don't remove IDs from SVGs, they are often used
-			// as hooks for embedding and styling
-			svgoPlugins: [{ cleanupIDs: false }]
-		})))
-		.pipe(gulp.dest('dist/images'));
-});
-
-gulp.task('extras', () => {
-	return gulp.src([
-		'app/*.*',
-		'!app/*.html'
-	], {
-		dot: true
-	}).pipe(gulp.dest('dist'));
-});
-
-gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
-
-gulp.task('serve', ['styles', 'scripts', 'fonts'], () => {
-	browserSync({
-		notify: false,
-		port: 9000,
-		server: {
-			baseDir: ['.tmp', 'app'],
-			routes: {
-				'/bower_components': 'bower_components'
-			}
-		}
-	});
-
-	gulp.watch([
-		'app/*.html',
-		'app/images/**/*',
-		'.tmp/fonts/**/*'
-	]).on('change', reload);
-
-	gulp.watch('app/styles/**/*.scss', ['styles']);
-	gulp.watch('app/scripts/**/*.js', ['scripts']);
-	gulp.watch('app/fonts/**/*', ['fonts']);
-});
-
-gulp.task('serve:dist', () => {
-	browserSync({
-		notify: false,
-		port: 9000,
-		server: {
-			baseDir: ['dist']
-		}
-	});
-});
-
-gulp.task('serve:test', ['scripts'], () => {
-	browserSync({
-		notify: false,
-		port: 9000,
-		ui: false,
-		server: {
-			baseDir: 'test',
-			routes: {
-				'/scripts': '.tmp/scripts',
-				'/bower_components': 'bower_components'
-			}
-		}
-	});
-
-	gulp.watch('app/scripts/**/*.js', ['scripts']);
-	gulp.watch('test/spec/**/*.js').on('change', reload);
-	gulp.watch('test/spec/**/*.js', ['lint:test']);
-});
-
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
-	return gulp.src('dist/**/*').pipe($.size({ title: 'build', gzip: true }));
-});
-
-gulp.task('default', ['clean'], () => {
-	GLOBAL.config.env = 'prod';
-	GLOBAL.config.notify = false;
-
-	gulp.start('build');
-});
-
-// -----------------------------------------------------------------------------
-
 gulp.task('styles', () => {
 	return gulp.src('client/styles/*.scss')
 		.pipe($.plumber())
@@ -207,18 +92,21 @@ gulp.task('fonts', () => {
 		.pipe(gulp.dest('.public/fonts'));
 });
 
-gulp.task('build:dev', ['styles', 'scripts', 'fonts'], () => {
-	// browserSync({
-	// 	notify: false,
-	// 	port: 9000,
-	// 	server: {
-	// 		baseDir: ['.tmp', 'app'],
-	// 		routes: {
-	// 			'/bower_components': 'bower_components'
-	// 		}
-	// 	}
-	// });
+gulp.task('images', () => {
+	return gulp.src('client/images/**/*')
+		.pipe($.cache($.imagemin({
+			progressive: true,
+			interlaced: true,
+			// don't remove IDs from SVGs, they are often used
+			// as hooks for embedding and styling
+			svgoPlugins: [{ cleanupIDs: false }]
+		})))
+		.pipe(gulp.dest('.public/images'));
+});
 
+gulp.task('clean', del.bind(null, ['.public', '.tmp', 'dist']));
+
+gulp.task('build:dev', ['styles', 'scripts', 'fonts', 'images'], () => {
 	gulp.watch([
 		'client/images/**/*',
 		'.public/fonts/**/*'
@@ -227,4 +115,16 @@ gulp.task('build:dev', ['styles', 'scripts', 'fonts'], () => {
 	gulp.watch('client/styles/**/*.scss', ['styles']);
 	gulp.watch('client/scripts/**/*.js', ['scripts']);
 	gulp.watch('client/fonts/**/*', ['fonts']);
+	gulp.watch('client/images/**/*', ['images']);
+});
+
+gulp.task('start:dev', ['build:dev'], () => {
+	$.nodemon({
+		script: 'server.js',
+		ext: 'hbs js',
+		tasks: ['bs']
+	})
+	.on('restart', () => {
+		console.log('[nodemon] restarted!');
+	});
 });
