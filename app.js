251e9eabca7dd14a28a2e70589d60f5bb4952228
RaduM <radu.micu@gmail.com>
2016-04-19 20:09:52 +0100

    Fix expressjs errors, removed unused files

--- app.js
+++ app.js
@@ -21,18 +21,6 @@ taunusExpress(taunus, app, {
 	layout: require('./.bin/views/layouts/main')
 });
 
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-
-// app.engine('hbs', exphbs({
-// 	defaultLayout: __dirname + '/views/layouts/main.hbs',
-// 	partialsDir: __dirname + '/views/partials',
-// 	layoutsDir: __dirname + '/views/layouts',
-// 	extname: 'hbs'
-// }));
-
-// app.set('view engine', 'hbs');
-
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -40,18 +28,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '.bin/public')));
-
-// app.use('/', routes);
-// app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-	var err = new Error('Not Found');
-
-	err.status = 404;
-
-	next(err);
-});
 
 // error handlers
 
